9e8c3013471bef424a580b33a30267e5e5958def
Salvo Giangreco <giangrecosalvo9@gmail.com>
2026-08-29 00:57:34 +0200

    unica: mods: tweaks: disable FM Radio country restrictions
    Change-Id: I52a69fc14a37e57032b3e0dc248074e96e8a8e0b
    
    # Conflicts:
    #	unica/mods/tweaks/customize.sh

--- unica/mods/tweaks/customize.sh
+++ unica/mods/tweaks/customize.sh
@@ -2,8 +2,12 @@
 # Guard the patch as the source firmware might have this already disabled
 LOG "- Applying \"Disable app compaction\" to /system/system/framework/services.jar"
 APPLY_PATCH "system" "system/framework/services.jar" \
-    "$MODPATH/appcompactor/services.jar/0001-Disable-app-compaction.patch" | true \
-    > /dev/null
+    "$MODPATH/appcompactor/services.jar/0001-Disable-app-compaction.patch" &> /dev/null || true
+
+# Disable FM Radio country restrictions
+if [ "$(GET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_FMRADIO_CONFIG_AVOID_REGION")" ]; then
+    SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_FMRADIO_CONFIG_AVOID_REGION" --delete
+fi
 
 # Show battery regulatory info in Settings
 # Requires SEM_BATTERY_PROPERTY_IC_AUTHENTICATION_RESULT support
